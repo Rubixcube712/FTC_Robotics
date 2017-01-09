a08f01f255f0264e263d3f681d58839064ba26f8
Hannah H <Rubixcube712@users.noreply.github.com>
2017-01-09 16:33:50 -0600

    Update driver_controlled.java

--- driver_controlled.java
+++ driver_controlled.java
@@ -1,3 +1,36 @@
+/*
+Copyright (c) 2016 Robert Atkinson
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Robert Atkinson nor the names of his contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -34,10 +67,7 @@ public class PushbotTeleopTank_Iterative extends OpMode{
 
     /* Declare OpMode members. */
     org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot robot       = new HardwarePushbot(); // use the class created to define a Pushbot's hardware
-                                                         // could also use HardwarePushbotMatrix class.
-    double          clawOffset  = 0.0 ;                  // Servo mid position
-    final double    CLAW_SPEED  = 0.02 ;                 // sets rate to move servo
-
+                                                                                                                // could also use HardwarePushbotMatrix class.
 
     /*
      * Code to run ONCE when the driver hits INIT
@@ -72,60 +102,47 @@ public class PushbotTeleopTank_Iterative extends OpMode{
      */
     @Override
     public void loop() {
-     
+
+
         //Drive forward
 
-        if (gamepad1.dpad_up) {
+        if (gamepad1.dpad_down) {
+            robot.backRight.setPower(1.0);
+            robot.frontRight.setPower(-1.0);
+
+            robot.frontLeft.setPower(1.0);
+            robot.backLeft.setPower(-1.0);
+        }
+        else if (gamepad1.b) {
             robot.backRight.setPower(1.0);
             robot.frontRight.setPower(1.0);
 
-            robot.frontLeft.setPower(-1 * 1.0);
-            robot.backLeft.setPower(-1 * 1.0);
-        }
-        else if (gamepad1.b) {
-            robot.backRight.setPower(0.5*1.0);
-            robot.frontRight.setPower(0.5*1.0);
-
-            robot.frontLeft.setPower(-1 * 0.5);
-            robot.backLeft.setPower(-1 * 0.5);
-        }
-        else {
-            robot.backLeft.setPower(0.0);
-            robot.backRight.setPower(0.0);
-
-            robot.frontLeft.setPower(0.0);
-            robot.frontRight.setPower(0.0);
+            robot.frontLeft.setPower(-1.0);
+            robot.backLeft.setPower(-1.0);
         }
 
+        //Backward
 
-        //Drive backward
+       else if (gamepad1.dpad_up) {
+            robot.backRight.setPower(-1.0);
+            robot.frontRight.setPower(1.0);
 
-        if (gamepad1.dpad_down) {
-            robot.backRight.setPower(-1 * 1.0);
-            robot.frontRight.setPower(-1 * 1.0);
+            robot.frontLeft.setPower(-1.0);
+            robot.backLeft.setPower(1.0);
+
+        }
+        else if (gamepad1.x) {
+            robot.backRight.setPower(-1.0);
+            robot.frontRight.setPower(-1.0);
 
             robot.frontLeft.setPower(1.0);
             robot.backLeft.setPower(1.0);
 
         }
-        else if (gamepad1.x) {
-            robot.backRight.setPower(-1 * 0.5);
-            robot.frontRight.setPower(-1 * 0.5);
-
-            robot.frontLeft.setPower(0.5*1.0);
-            robot.backLeft.setPower(0.5*1.0);
-
-        }
-        else {
-            robot.backLeft.setPower(0.0);
-            robot.backRight.setPower(0.0);
-            robot.frontLeft.setPower(0.0);
-            robot.frontRight.setPower(0.0);
-        }
 
         //Turn Right
 
-        if (gamepad1.dpad_right) {
+        else if (gamepad1.dpad_left) {
             robot.backLeft.setPower(1.0);
             robot.frontLeft.setPower(1.0);
 
@@ -134,35 +151,30 @@ public class PushbotTeleopTank_Iterative extends OpMode{
 
         }
         else if (gamepad1.a) {
-            robot.backLeft.setPower(0.5);
-            robot.frontLeft.setPower(0.5);
+            robot.backLeft.setPower(1.0);
+            robot.frontLeft.setPower(1.0);
 
-            robot.frontRight.setPower(0.5);
-            robot.backRight.setPower(0.5);
-        }
-        else {
-            robot.backLeft.setPower(0.0);
-            robot.backRight.setPower(0.0);
-            robot.frontLeft.setPower(0.0);
-            robot.frontRight.setPower(0.0);
+            robot.frontRight.setPower(1.0);
+            robot.backRight.setPower(1.0);
         }
 
         //Turn Left
 
-        if (gamepad1.dpad_left) {
-            robot.backLeft.setPower(-1*1.0);
-            robot.frontLeft.setPower(-1*1.0);
+        else if (gamepad1.dpad_right) {
+            //robot.leftMotor.setDirection(DcMotor.Direction.FORWARD);
+            robot.backLeft.setPower(-1.0);
+            robot.frontLeft.setPower(-1.0);
 
-            robot.backRight.setPower(-1*1.0);
-            robot.frontRight.setPower(-1*1.0);
+            robot.backRight.setPower(-1.0);
+            robot.frontRight.setPower(-1.0);
 
         }
         else if (gamepad1.y) {
-            robot.backLeft.setPower(-1*0.5);
-            robot.frontLeft.setPower(-1*0.5);
+            robot.backLeft.setPower(-1.0);
+            robot.frontLeft.setPower(-1.0);
 
-            robot.frontRight.setPower(-1*0.5);
-            robot.backRight.setPower(-1*0.5);
+            robot.frontRight.setPower(-1.0);
+            robot.backRight.setPower(-1.0);
         }
         else {
             robot.backLeft.setPower(0.0);
@@ -170,14 +182,15 @@ public class PushbotTeleopTank_Iterative extends OpMode{
             robot.frontLeft.setPower(0.0);
             robot.frontRight.setPower(0.0);
         }
-        
     }
 
     /*
      * Code to run ONCE after the driver hits STOP
      */
+
     @Override
     public void stop() {
     }
 
 }
+
