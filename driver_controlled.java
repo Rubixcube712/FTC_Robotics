bf3d7410c474ff8a35a85f42fbf803ccac6a9ef3
Hannah H <Rubixcube712@users.noreply.github.com>
2017-01-14 07:30:45 -0600

    Update driver_controlled.java

--- driver_controlled.java
+++ driver_controlled.java
@@ -1,196 +1,123 @@
-/*
-Copyright (c) 2016 Robert Atkinson
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted (subject to the limitations in the disclaimer below) provided that
-the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this list
-of conditions and the following disclaimer.
-
-Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-Neither the name of Robert Atkinson nor the names of his contributors may be used to
-endorse or promote products derived from this software without specific prior
-written permission.
-
-NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
-
 /**
- * This file provides basic Telop driving for a Pushbot robot.
- * The code is structured as an Iterative OpMode
+ * This is NOT an opmode.
  *
- * This OpMode uses the common Pushbot hardware class to define the devices on the robot.
- * All device access is managed through the HardwarePushbot class.
+ * This class can be used to define all the specific hardware for a single robot.
+ * In this case that robot is a Pushbot.
+ * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
  *
- * This particular OpMode executes a basic Tank Drive Teleop for a PushBot
- * It raises and lowers the claw using the Gamepad Y and A buttons respectively.
- * It also opens and closes the claws slowly using the left and right Bumper buttons.
+ * This hardware class assumes the following device names have been configured on the robot:
+ * Note:  All names are lower case and some have single spaces between words.
  *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ * Motor channel:  Left  drive motor:        "left_drive"
+ * Motor channel:  Right drive motor:        "right_drive"
+ * Motor channel:  Manipulator drive motor:  "left_arm"
+ * Servo channel:  Servo to open left claw:  "left_hand"
+ * Servo channel:  Servo to open right claw: "right_hand"
  */
+public class HardwarePushbot
+{
+    /* Public OpMode members. */
+    public DcMotor  leftMotor   = null;
+    public DcMotor  rightMotor  = null;
+    public DcMotor  FrontLeftMotor    = null;
+    public Servo    leftClaw    = null;
+    public Servo    rightClaw   = null;
+    public DcMotor  frontLeft   = null;
+    public DcMotor  backRight  = null;
+    public DcMotor  backLeft    = null;
+    public DcMotor  frontRight  = null;
 
-@TeleOp(name="Pushbot: Teleop Tank", group="Pushbot")
-//@Disabled
-public class PushbotTeleopTank_Iterative extends OpMode{
+    public DcMotor  arm  = null;
 
-    /* Declare OpMode members. */
-    org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot robot       = new HardwarePushbot(); // use the class created to define a Pushbot's hardware
-                                                                                                                // could also use HardwarePushbotMatrix class.
+    //public DcMotor  mainArm  = null;
+    //public DcMotor  secondArm  = null;
 
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
-    @Override
-    public void init() {
-        /* Initialize the hardware variables.
-         * The init() method of the hardware class does all the work here
-         */
-        robot.init(hardwareMap);
+    public static final double MID_SERVO       =  0.5 ;
+    public static final double ARM_UP_POWER    =  0.75 ;
+    public static final double ARM_DOWN_POWER  = -0.75 ;
 
-        // Send telemetry message to signify robot waiting;
-        telemetry.addData("Say", "Hello Driver");    //
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public HardwarePushbot(){
+
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
-    @Override
-    public void init_loop() {
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        frontLeft   = hwMap.dcMotor.get("LeftFront");
+        //backRight  = hwMap.dcMotor.get("RightBack");
+        //backLeft    = hwMap.dcMotor.get("LeftBack");
+        frontRight = hwMap.dcMotor.get("RightFront");
+        arm = hwMap.dcMotor.get("arm");
+
+        //mainArm    = hwMap.dcMotor.get("mainArm");
+        //secondArm = hwMap.dcMotor.get("secondArm");
+
+        // Set all motors to zero power
+        frontLeft.setPower(0);
+        //backRight.setPower(0);
+        //backLeft.setPower(0);
+        frontRight.setPower(0);
+        arm.setPower(0);
+        //mainArm.setPower(0);
+        //secondArm.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        //mainArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //secondArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+//        // Define and initialize ALL installed servos.
+//        leftClaw = hwMap.servo.get("RightBack");
+//        rightClaw = hwMap.servo.get("RightBack");
+//        leftClaw.setPosition(MID_SERVO);
+//        rightClaw.setPosition(MID_SERVO);
     }
 
-    /*
-     * Code to run ONCE when the driver hits PLAY
+    /***
+     *
+     * waitForTick implements a periodic delay. However, this acts like a metronome with a regular
+     * periodic tick.  This is used to compensate for varying processing times for each cycle.
+     * The function looks at the elapsed cycle time, and sleeps for the remaining time interval.
+     *
+     * @param periodMs  Length of wait cycle in mSec.
      */
-    @Override
-    public void start() {
+    public void waitForTick(long periodMs) {
+
+        long  remaining = periodMs - (long)period.milliseconds();
+
+        // sleep for the remaining portion of the regular cycle period.
+        if (remaining > 0) {
+            try {
+                Thread.sleep(remaining);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        // Reset the cycle clock for the next pass.
+        period.reset();
     }
-
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
-    @Override
-    public void loop() {
-
-
-        //Drive forward
-
-        if (gamepad1.dpad_down) {
-            robot.backRight.setPower(1.0);
-            robot.frontRight.setPower(-1.0);
-
-            robot.frontLeft.setPower(1.0);
-            robot.backLeft.setPower(-1.0);
-        }
-        else if (gamepad1.b) {
-            robot.backRight.setPower(1.0);
-            robot.frontRight.setPower(1.0);
-
-            robot.frontLeft.setPower(-1.0);
-            robot.backLeft.setPower(-1.0);
-        }
-
-        //Backward
-
-       else if (gamepad1.dpad_up) {
-            robot.backRight.setPower(-1.0);
-            robot.frontRight.setPower(1.0);
-
-            robot.frontLeft.setPower(-1.0);
-            robot.backLeft.setPower(1.0);
-
-        }
-        else if (gamepad1.x) {
-            robot.backRight.setPower(-1.0);
-            robot.frontRight.setPower(-1.0);
-
-            robot.frontLeft.setPower(1.0);
-            robot.backLeft.setPower(1.0);
-
-        }
-
-        //Turn Right
-
-        else if (gamepad1.dpad_left) {
-            robot.backLeft.setPower(1.0);
-            robot.frontLeft.setPower(1.0);
-
-            robot.backRight.setPower(1.0);
-            robot.frontRight.setPower(1.0);
-
-        }
-        else if (gamepad1.a) {
-            robot.backLeft.setPower(1.0);
-            robot.frontLeft.setPower(1.0);
-
-            robot.frontRight.setPower(1.0);
-            robot.backRight.setPower(1.0);
-        }
-
-        //Turn Left
-
-        else if (gamepad1.dpad_right) {
-            //robot.leftMotor.setDirection(DcMotor.Direction.FORWARD);
-            robot.backLeft.setPower(-1.0);
-            robot.frontLeft.setPower(-1.0);
-
-            robot.backRight.setPower(-1.0);
-            robot.frontRight.setPower(-1.0);
-
-        }
-        else if (gamepad1.y) {
-            robot.backLeft.setPower(-1.0);
-            robot.frontLeft.setPower(-1.0);
-
-            robot.frontRight.setPower(-1.0);
-            robot.backRight.setPower(-1.0);
-        }
-        else {
-            robot.backLeft.setPower(0.0);
-            robot.backRight.setPower(0.0);
-            robot.frontLeft.setPower(0.0);
-            robot.frontRight.setPower(0.0);
-        }
-    }
-
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
-
-    @Override
-    public void stop() {
-    }
-
 }
+
 
